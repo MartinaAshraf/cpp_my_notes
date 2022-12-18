592b39f81eb259d8431db2afd4784dd8b595b21c
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2022-12-19 01:07:12 +0200

    Update factorial.cpp

--- study/factorial.cpp
+++ study/factorial.cpp
@@ -11,4 +11,17 @@ int main()
 	}
 	cout <<"fact "<< fact;
 
-  
+  // another sloution
+        #include <iostream>
+using namespace std;
+int main()
+{
+	int n , fact=1,i=1;
+	cin >> n;
+	while (i <= n )
+	{
+		fact = fact * i;
+		i++;
+	}
+	cout <<"fact "<< fact;
+}
