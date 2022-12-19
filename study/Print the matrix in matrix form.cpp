230ca6ab831fed61c465267c701283fe1da071e2
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2022-12-19 02:35:58 +0200

    Create Print the matrix in matrix form.cpp

---
+++ study/Print the matrix in matrix form.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int list[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+for (int i = 0; i < 3; i++)
+{
+for (int j = 0; j < 3; j++)
+{
+cout << list[i][j] << " ";
+}
+cout << endl;
+}
+return 0;
+}
