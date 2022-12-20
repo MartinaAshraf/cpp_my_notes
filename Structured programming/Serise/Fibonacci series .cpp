fc2f4bcb0ae4fe3b75be2d35b79a4b7be6f162f8
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2022-12-21 01:14:03 +0200

    Create Fibonacci series .cpp

---
+++ Structured programming/Serise/Fibonacci series .cpp
@@ -0,0 +1,28 @@
+//0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+//الرقم التالى = مجموع الرقمين السابقين -->8=3+5
+#include <iostream>
+using namespace std;
+int main()
+{
+	int index, second_num = 0, frist_num = 0, total = 0;
+	cin >> index; //2
+	for (int i = 0; i <= index; i++) //i=0 .. i=1 .. i=2
+	{
+		if (i== 0)
+		{
+			frist_num = 0;
+			cout << frist_num << " , "; //0 ,
+			continue;
+		}
+		else if (i == 1)
+		{
+			second_num = 1;
+			cout <<  second_num << " , ";//0 , 1
+			continue;
+		}
+		total = second_num + frist_num;// 1+0=1
+		frist_num = second_num;
+		second_num = frist_num;
+		cout << total << " , ";//0 , 1 , 1 
+	}
+}
