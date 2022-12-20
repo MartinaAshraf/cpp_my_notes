d3b70834a5f51373977739b90ac5470ec51bf5d7
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2022-12-21 00:47:24 +0200

    Create Palindrome String.cpp

---
+++ Structured programming/Palindrome/Palindrome String.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+//Ipalind rome string ==> level
+	string s;
+
+cout << "enter a string: ";
+cin >> s;
+
+bool isPalindrome = true;
+
+for (int i = 0; i < (s.length() / 2); i++)
+{
+	if (s[i] != s[s.length() - 1 - i])
+	{
+		isPalindrome = false;
+	}
+}
+cout << boolalpha<< isPalindrome;
+
+}
