8de969b299876fbd9de22924111afc6ce85c559b
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2022-12-21 01:17:57 +0200

    Update Fibonacci series .cpp

--- Structured programming/Serise/Fibonacci series .cpp
+++ Structured programming/Serise/Fibonacci series .cpp
@@ -5,24 +5,25 @@ using namespace std;
 int main()
 {
 	int index, second_num = 0, frist_num = 0, total = 0;
+	cout << "Enter the index : ";
 	cin >> index; //2
 	for (int i = 0; i <= index; i++) //i=0 .. i=1 .. i=2
 	{
 		if (i== 0)
 		{
 			frist_num = 0;
-			cout << frist_num << " , "; //0 ,
+			cout << frist_num << "  "; //0 
 			continue;
 		}
 		else if (i == 1)
 		{
 			second_num = 1;
-			cout <<  second_num << " , ";//0 , 1
+			cout <<  second_num << "  ";//0  1
 			continue;
 		}
 		total = second_num + frist_num;// 1+0=1
 		frist_num = second_num;
 		second_num = frist_num;
-		cout << total << " , ";//0 , 1 , 1 
+		cout << total << "  ";//0  1  1 
 	}
 }
