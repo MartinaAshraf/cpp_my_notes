e7d4315c92684872d9ae43af105031a622d60b8a
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2022-12-21 13:23:36 +0200

    Create The greater value between two numbers.cpp

---
+++ Structured programming/compare/The greater value between two numbers.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int x,y;
+cout<<"enter x value";
+cin>>x;
+cout<<"enter y value";
+cin>>y;
+if(x>y)
+{
+cout<<"x is great";
+}
+else
+{
+cout<<"y is great";
+}
+return 0;
+}
