91fadf9b40d1eb1b003f0e7b39132cd6c34a0f01
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2022-12-21 14:19:24 +0200

    Create Right-angled triangle on left side.cpp

---
+++ Structured programming/shapes/Right-angled triangle on left side.cpp
@@ -0,0 +1,74 @@
+/*
+
+*
+**
+***
+****
+*****
+
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+for(int i=1;i<=5;i++)
+{
+for(int j=1;j<=i;j++)
+{
+cout<<"*";
+}
+cout<<endl;
+}
+return 0;
+}
+
+
+
+
+
+/*
+1
+22
+333
+4444
+55555
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+for(int i=1;i<=5;i++)
+{
+for(int j=1;j<=i;j++)
+{
+cout<<i;
+}
+cout<<endl;
+}
+return 0;
+}
+
+
+/*
+1
+12
+123
+1234
+12345
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+for(int i=1;i<=5;i++)
+{
+for(int j=1;j<=i;j++)
+{
+cout<<j;
+}
+cout<<endl;
+}
+return 0;
+}
+
+
