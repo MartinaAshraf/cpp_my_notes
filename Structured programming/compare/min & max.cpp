dc55620c5b7ac50a7b9c4bc0dd02429e804c04e8
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2022-12-21 13:51:57 +0200

    Update min & max.cpp

--- Structured programming/compare/min & max.cpp
+++ Structured programming/compare/min & max.cpp
@@ -1,11 +1,14 @@
+// frist selution by using : FOR 
 #include <iostream>
 using namespace std;
 int main()
 {
 	int counter, x, max = INT_MIN, min = INT_MAX;
+	cout << "enter the counter :  " ;
 	cin >> counter;
 	for (int i = 0; i < counter; i++)
 	{
+		cout<< "enter the value : ";
 		cin >> x;
 
 		if (x> max)
@@ -20,5 +23,41 @@ int main()
 		}
 		
 	}
-	cout <<"min num = " << min << "  " <<"max num = " << max;
+	cout <<"min num = " << min << endl <<"max num = " << max << endl;
+}
+
+
+// Another selution by using : ARRAY
+
+#include<iostream>
+using namespace std;
+int main()
+{
+int size;
+cout << "Enter the size of array : ";
+	cin >> size;
+	int* arr = new int[size];
+	for (int i = 0; i < size; i++)
+	{
+	    cout<<"enter the value : "; 
+	    cin>>arr[i];
+	}
+
+
+int max=arr[0];
+int min=arr[0];
+for(int i=0;i<5;i++)
+{
+if(arr[i]>max)
+{
+max=arr[i];
+}
+if(arr[i]<min)
+{
+min=arr[i];
+}
+}
+cout<<"max = "<<max<<endl;
+cout<<"min = "<<min<<endl;
+return 0;
 }
