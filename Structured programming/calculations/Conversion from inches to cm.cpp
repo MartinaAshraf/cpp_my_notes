849d6f8ab444acf80ec65b81bb564afaf58da460
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2022-12-21 03:39:22 +0200

    Create Conversion from inches to cm.cpp

---
+++ Structured programming/calculations/Conversion from inches to cm.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float inch;
+    cout << "enter inch";
+    cin >> inch;
+    float cm = 2.5*inch;
+    cout << "cm = " << cm;
+    return 0;
+}
