17ce6aa4b9001887075de2e42ab729a25eaa9953
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2022-12-21 03:36:21 +0200

    Create calculate (2a-4)^7.cpp

---
+++ Structured programming/calculations/calculate (2a-4)^7.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "enter a value" << endl;
+    cin >> a;
+    int z =pow((2*a-4),7) ;
+    cout << "z=" << z << endl;
+    return 0;
+}
