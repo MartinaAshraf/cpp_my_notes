a307b6d7d5d4e987431881fe506441e60f9d23ba
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2023-01-01 23:22:09 +0200

    Create 46.cpp

---
+++ Structured programming/50 Problems/46.cpp
@@ -0,0 +1,17 @@
+/*Write a C++ program to read any two real numbers (which you may assume are not equal),
+and write out the larger of the two with a suitable message.
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	float  num1,num2;
+	cout << "Enter the frist number   = ";
+	cin >> num1;
+	cout << "Enter the second number = ";
+	cin >> num2;
+	if (num1 > num2) { cout << " The biggest number is : " << num1<<endl; }
+	if (num1 < num2) { cout << " The biggest number is : " << num2 << endl; }
+	else { cout << " The two numbers are equal"; }
+  
+}
