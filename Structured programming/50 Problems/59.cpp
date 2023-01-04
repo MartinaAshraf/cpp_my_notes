026a45796d4755df9e24a23bf22cd86d8fce9a35
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2023-01-04 21:26:43 +0200

    Create 59.cpp

---
+++ Structured programming/50 Problems/59.cpp
@@ -0,0 +1,28 @@
+/*The transcendental number e (2.718281828 ...) can be shown to be the limit of
+1/(1 − 𝑥)^(1/x)
+as x tends to zero (from above).
+Write a C++ program which shows how this expression converges to e as x gets closer 
+and closer to zero (use the real kind with the greatest precision available on your system).
+*/
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main() {
+double e = exp(1);
+cout << fixed << setprecision(7);
+cout << " e = " << e << "\n";
+double x = 1.0;
+double res = 3.0;
+double eps = 1e-8;
+while (res - e >= eps) 
+{
+res = 1 / pow((1.0 - x), 1 / x);
+x /= 10;
+cout << fixed << setprecision(9);
+cout << "when x = " << x << " ===>> ";
+cout << fixed << setprecision(7);
+cout << "result = " << res;
+cout << "\n";
+}
+}
