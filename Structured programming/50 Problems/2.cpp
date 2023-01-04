648d0364c23e6268f0f252a79d0d8a97a50c933f
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2023-01-04 22:40:50 +0200

    Update 2.cpp

--- Structured programming/50 Problems/2.cpp
+++ Structured programming/50 Problems/2.cpp
@@ -3,7 +3,7 @@ compute this number, bearing in mind that an attempt to compute 1031 will cause 
 error.
 */
 
-2
+
 #include <iostream>
 #include<cmath>
 #include<algorithm>
@@ -11,16 +11,16 @@ Using namespace std;
 
 Int main()
 {
-    Int r,h;//3,4
-    Double A,S,Sc,V;
-    Cin>>r>>h;
+    int r,h;//3,4
+    double A,S,Sc,V;
+    cin>>r>>h;
     A=3.14*r*r;
     S=sqrt(4*3.14*A);
     Sc=2*3.14*(r*r+r*h);
     V=3.14*r*r*h;
-    Cout<<”area of a circle = “<<A<<endl;//28.26
-    Cout<<”circumference of a circle = “<<S<<endl;//18.84
-    Cout<<”surface area of a cylinder = “<<Sc<<endl;//131.88
-    Cout<<”volume of a cylinder = “<<V<<endl;//113,04
+    cout<<”area of a circle = “<<A<<endl;//28.26
+    cout<<”circumference of a circle = “<<S<<endl;//18.84
+    cout<<”surface area of a cylinder = “<<Sc<<endl;//131.88
+    cout<<”volume of a cylinder = “<<V<<endl;//113,04
     return 0;
 }
