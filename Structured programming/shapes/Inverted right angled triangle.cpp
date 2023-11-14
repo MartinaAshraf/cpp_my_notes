4ee982aeca929cb730dd389322df514346813756
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2023-11-14 21:28:07 +0200

    Update Inverted right angled triangle.cpp

--- Structured programming/shapes/Inverted right angled triangle.cpp
+++ Structured programming/shapes/Inverted right angled triangle.cpp
@@ -54,7 +54,77 @@ return 0;
 using namespace std;
 int main()
 {
+for(int i=5;i>=1;i--)
+{
+for(int j=1;j<=i;j++)
+{
+cout<<j;
+}
+cout<<endl;
+}
+return 0;
+}
+
+
+
+/*
+54321
+5432
+543
+54
+5
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
 for(int i=1;i<=5;i++)
+{
+for(int j=5;j>=i;j--)
+{
+cout<<j;
+}
+cout<<endl;
+}
+return 0;
+}
+
+
+/*
+1
+12
+123
+1234
+12345
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+for(int i=1;i<=5;i++)
+{
+for(int j=1;j<=i;j++)
+{
+cout<<j;
+}
+cout<<endl;
+}
+return 0;
+}
+
+
+/*
+5
+54
+543
+5432
+54321
+  */
+#include <iostream>
+using namespace std;
+int main()
+{
+for(int i=5;i>=1;i--)
 {
 for(int j=5;j>=i;j--)
 {
