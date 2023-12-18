ac4b841072b669c2e437ed8dc0acc333a30cd25a
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2023-12-18 18:38:31 +0200

    Update triangle stars .cpp

--- Structured programming/shapes/triangle stars .cpp
+++ Structured programming/shapes/triangle stars .cpp
@@ -20,3 +20,30 @@ cout<<"*";
 } } cout<<endl;
 }
 return 0;}
+
+/*
+
+*      
+**     
+* *    
+*  *   
+*   *  
+*    * 
+*******
+
+*/
+#include <iostream>
+using namespace std;
+int main() {
+int i,j; for(i=1 ; i <= 7 ; i++) {
+for(j=1 ; j <= 7 ; j++) {
+if(j == 1 or i == j or i == 7) {
+cout<<"*";
+}
+ else
+ { cout<<" ";} 
+}
+ cout<<endl;
+} 
+ return 0;
+}
