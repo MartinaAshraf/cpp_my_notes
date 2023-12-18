6756446ab90f17787dee5e32c838a1c64ce792f3
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2023-12-18 18:35:43 +0200

    Create triangle stars .cpp

---
+++ Structured programming/shapes/triangle stars .cpp
@@ -0,0 +1,22 @@
+/*
+
+*******
+ *    *
+  *   *
+   *  *
+    * *
+     **
+      *
+
+*/
+#include <iostream> 
+using namespace std;
+int main() {
+int i,j; for(i=1 ; i <= 7 ; i++) {
+for(j=1 ; j <= 7 ; j++) {
+if(i == 1 or i == j or j == 7) {
+cout<<"*";
+}else{ cout<<" ";
+} } cout<<endl;
+}
+return 0;}
